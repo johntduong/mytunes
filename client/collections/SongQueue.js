eca8bcb34939484f82f204cf1da87ef84f05815a
johntduong <johntduong@yahoo.com>
2017-02-20 20:04:11 -0800

    Pass test for SongQueue up to when a song ends

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,15 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', () => {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+  },
+  playFirst: function() {
+    console.log(this['model']);
+    this['models']['0'].__proto__.play();
   }
 
 });
